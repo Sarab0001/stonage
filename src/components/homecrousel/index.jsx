d6821b0c3de862f2b2edf2e91e603897ab65da22
devashishHp04 <devashish04.hp@gmail.com>
2024-05-31 20:11:14 +0530

    add autoplay in banner sections

--- src/components/homecrousel/index.jsx
+++ src/components/homecrousel/index.jsx
@@ -1,10 +1,11 @@
-'use client'
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { sliderImage } from '@/constant';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { sliderImage } from "@/constant";
+import Link from "next/link";
+import { Autoplay } from "swiper/modules";
 
 export default function Homecrousel({ hideText, color }) {
   return (
@@ -14,7 +15,12 @@ export default function Homecrousel({ hideText, color }) {
         centeredSlides={false}
         spaceBetween={15}
         loop={true}
-
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        modules={[Autoplay]}
         scrollbar={{
           hide: true,
         }}
@@ -42,30 +48,37 @@ export default function Homecrousel({ hideText, color }) {
         }}
         className={color ? `swiper-containers ` : "swiper-container"}
       >
-        <div className=' '>
-
+        <div className=" ">
           {sliderImage.map((elem, i) => (
             <SwiperSlide key={i}>
-              <div className='lg:mt-8'>
+              <div className="lg:mt-8">
                 {elem.image ? (
-                  <img className="md:w-300 md:h-300 lg:w-346 relative z-50 lg:h-346 2xl:w-550 2xl:h-346 w-278 h-278 object-cover" src={elem.image.src} alt="image" />
+                  <img
+                    className="md:w-300 md:h-300 lg:w-346 relative z-50 lg:h-346 2xl:w-550 2xl:h-346 w-278 h-278 object-cover"
+                    src={elem.image.src}
+                    alt="image"
+                  />
                 ) : (
-
-                  <video className='object-cover md:h-300 lg:h-346 w-346 h-278 2xl:w-550 2xl:h-346 relative z-50' preload="none" autoPlay={true} muted playsInline loop>
+                  <video
+                    className="object-cover md:h-300 lg:h-346 w-346 h-278 2xl:w-550 2xl:h-346 relative z-50"
+                    preload="none"
+                    autoPlay={true}
+                    muted
+                    playsInline
+                    loop
+                  >
                     <source src={elem.video} type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
-
                 )}
-
               </div>
               {hideText && (
                 <>
-                  <h1 className="xs:py-2 py-1 text-black font-semibold">{elem.title}</h1>
-                  <p className='text-black text-15 font-serif'>{elem.desc}</p>
-                  <div className='lg:h-300  h-[150px]'></div>
-
-
+                  <h1 className="xs:py-2 py-1 text-black font-semibold">
+                    {elem.title}
+                  </h1>
+                  <p className="text-black text-15 font-serif">{elem.desc}</p>
+                  <div className="lg:h-300  h-[150px]"></div>
                 </>
               )}
             </SwiperSlide>
